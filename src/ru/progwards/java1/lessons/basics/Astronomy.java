8c9ec2e2719886084c035aa9617a6e8ec5a826c0
Mikhail <t9154360684@yandex.ru>
2020-12-04 19:19:30 +0300

    Begin

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -1,15 +1,41 @@
 package ru.progwards.java1.lessons.basics;
 
 public class Astronomy {
-    final double PI = 3.14;
+    public static final double PI = 3.14;
 
     public static Double sphereSquare(Double r) {
-        double r2 = r*r;
+        double r2 = r * r;
         double result = 4.0 * PI * r2;
         return result;
     }
 
+    public static Double earthSquare() {
+        return sphereSquare(6371.2);
+    }
+
+    public static Double mercurySquare() {
+        return sphereSquare(2439.7);
+    }
+
+    public static Double jupiterSquare() {
+        return sphereSquare(71492.0);
+    }
+
+    public static Double earthVsMercury() {
+        return sphereSquare(mercurySquare() - earthSquare());
+    }
+
+    public static Double earthVsJupiter() {
+        return sphereSquare(jupiterSquare() - earthSquare());
+    }
+
+
     public static void main(String[] args) {
         System.out.println(sphereSquare(6371.2));
+        System.out.println(earthSquare());
+        System.out.println(mercurySquare());
+        System.out.println(jupiterSquare());
+        System.out.println(earthVsMercury());
+        System.out.println(earthVsJupiter());
     }
 }
