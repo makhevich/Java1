1199f5d2ed4f95c150ae97bc89658f2d036c1469
Mikhail <t9154360684@yandex.ru>
2020-12-09 17:23:31 +0300

    Begin

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -0,0 +1,14 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class CyclesGoldenFibo {
+
+    public static boolean containsDigit(int number, int digit) {
+        return false;
+
+    }
+
+    public static int fiboNumber(int n) {
+        for (int i = 0; i >= n; i++);
+
+    }
+}
