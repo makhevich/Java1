3cffaeeca5e8bb5d38801d06f4beddd49bd413a8
Mikhail <t9154360684@yandex.ru>
2020-12-09 17:07:53 +0300

    Begin

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -20,11 +20,17 @@ public class TriangleInfo {
         int sideTriangle1 = a * a;
         int sideTriangle2 = b * b;
         int sideTriangle3 = c * c;
-        if (sideTriangle1 == (sideTriangle2 + sideTriangle3))
-            result = true;
-        if (sideTriangle2 == (sideTriangle1 + sideTriangle3))
-            result = true;
-        if (sideTriangle3 == (sideTriangle1 + sideTriangle2))
+        if (sideTriangle1 == (sideTriangle2 + sideTriangle3) || sideTriangle2 == (sideTriangle1 + sideTriangle3)
+                || sideTriangle3 == (sideTriangle1 + sideTriangle2))
+                result = true;
+        else
+            result = false;
+        return result;
+    }
+
+    public static boolean isIsoscelesTriangle(int a, int b, int c) {
+        boolean result;
+        if (a == b || a == c || b == c)
             result = true;
         else
             result = false;
@@ -34,7 +40,8 @@ public class TriangleInfo {
 
     public static void main(String[] args) {
         System.out.println(isTriangle(100,10,10));
-        System.out.println(isRightTriangle(4,3,4));
+        System.out.println(isRightTriangle(5,3,4));
+        System.out.println(isIsoscelesTriangle(8,5,5));
     }
 
 }
